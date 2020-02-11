e54972ef4d08e51365aecd03101805dde989515e
Sarath Kaul <sarath.kaul@searce.com>
2020-02-11 22:12:07 +0530

    Dependencies Installed

---
+++ app.js
@@ -0,0 +1,4 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+const moment = require('moment');

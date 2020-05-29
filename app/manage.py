81605745276e8282a92b15a1329ef950ddbfa2a2
Sarath Kaul <kaul.sarath@gmail.com>
2020-05-29 10:08:02 +0000

    DB MIgration Changes

--- app/manage.py
+++ app/manage.py
@@ -1,9 +1,18 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-from backend import app,db
+app = Flask(__name__, static_folder='', static_url_path='')
+app.config.from_object(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL"),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
 
+# initialize the database connection
+db = SQLAlchemy(app)
 app.config.from_object(__name__)
 
 migrate = Migrate(app, db)

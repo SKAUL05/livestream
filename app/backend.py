2785e8cdc590395bfab5e7932f8f13ea30af3f9c
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-07-31 00:25:51 +0530

    changed code to Black format

--- app/backend.py
+++ app/backend.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from .models import Ideas
 import time
 
-app = Flask(__name__, static_folder='', static_url_path='')
+app = Flask(__name__, static_folder="", static_url_path="")
 app.config.from_object(__name__)
 app.config.update(
     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL"),
@@ -32,8 +32,6 @@ db = SQLAlchemy(app)
 #         print("App database already exists. Run the app without --setup.")
 #     finally:
 #         connection.close()
-
-
 
 
 # Managing connections
@@ -60,27 +58,29 @@ def teardown_request(exception):
         db.session.rollback()
     db.session.remove()
 
+
 def retr_dict(obj=None):
     return_dict = {}
     if obj:
-        return_dict['id'] = obj.id
-        return_dict['text'] = obj.text
-        return_dict['tech'] = obj.tech
-        return_dict['viewer'] = obj.viewer
-        return_dict['count'] = obj.upVote
+        return_dict["id"] = obj.id
+        return_dict["text"] = obj.text
+        return_dict["tech"] = obj.tech
+        return_dict["viewer"] = obj.viewer
+        return_dict["count"] = obj.upVote
         if obj.time:
-            readable = obj.time + timedelta(hours = 5) +timedelta(minutes = 30)
-            return_dict['time'] = readable.strftime("%d-%B-%Y, %I:%M:%S %p")
+            readable = obj.time + timedelta(hours=5) + timedelta(minutes=30)
+            return_dict["time"] = readable.strftime("%d-%B-%Y, %I:%M:%S %p")
         else:
-            retr_dict['time'] = "N/A"
+            retr_dict["time"] = "N/A"
     return return_dict
+
 
 @app.route("/livestream", methods=["GET"])
 def get_todos():
     print("In Here......")
     try:
         selection = Ideas.query.order_by(Ideas.id).all()
-        print("Objects %s ",selection)
+        print("Objects %s ", selection)
         return_list = []
         for obj in selection:
             return_list.append(retr_dict(obj=obj))
@@ -90,31 +90,33 @@ def get_todos():
         print(e)
         return json.dumps({})
 
+
 @app.route("/livestream", methods=["POST"])
 def new_todo():
     print("Todo......")
     entry_data = request.json
-    print(entry_data['text'])
+    print(entry_data["text"])
     idea = Ideas(entry_data)
     db.session.add(idea)
     db.session.commit()
     print("Here......")
     return jsonify(id=idea.id)
 
-@app.route("/upvote", methods = ['POST'])
+
+@app.route("/upvote", methods=["POST"])
 def add_upvote():
     print("Upvote")
     u_data = request.json
-    obj = Ideas.query.filter_by(id = u_data['id']).first()
+    obj = Ideas.query.filter_by(id=u_data["id"]).first()
     print(obj.upVote)
     print(type(obj.upVote))
     if obj.upVote is not None:
         obj.upVote += 1
     else:
         obj.upVote = 1
-        
+
     db.session.commit()
-    return jsonify(count = obj.upVote)
+    return jsonify(count=obj.upVote)
 
 
 @app.route("/")
@@ -122,8 +124,5 @@ def show_todos():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-
-
-
